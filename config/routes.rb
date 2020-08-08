07d4099b6db6368275ff1a8998d1150138385f48
vagrant <vagrant@localhost.localdomain>
2020-08-08 03:31:53 +0000

    Add search func

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about'
+  get '/search', to: 'search#search'
+
   resources :users, only: [:show, :index, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'follows' => 'relationships#follower', as: 'follows'
